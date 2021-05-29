a6db770da9d1caba89d27a74b7dc1884ed0d9737
Faruq2014 <mjmolla@gmail.com>
2021-05-29 12:01:03 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+//data types
+ // basicly two types, String and Number
+ // String
+// String is the alphabetical data type. anything between ' ' and " " is a String. How long colld 
+// could be the string = no limitation.
+// string could be mix and match.
+
+let name="Faruq 2020 @4!"
+console.log(name);
+// number 
+// if your value is number, than js will automateclly decated that it is a number type data.
+// you do not declare the scope of the data type. 
+var y=4.6;
+var z= 45;
+const x=y+z;
+console.log(x);
+console.log("hello how are you")
+
+// variables= variable is a place holder.
+//var firstName="Faruq";
+var firstName="Shafik";
+var lastName='Molla'
+var age=40;
+// var= variable, firstName= variable name, = operators sign, Faruq=value.
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+// concatination
+console.log("my first name is "+firstName)
+console.log("my last name is "+lastName)
+var fullName= firstName+" "+lastName;
+console.log("my full name is "+fullName)
+
+
+
+
